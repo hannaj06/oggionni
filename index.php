7759c3949cc5418544a990503e4c813ccca5f6d3
hannaj06 <joseph.hanna10@gmail.com>
2016-09-24 21:44:38 -0400

    db_connect

--- index.php
+++ index.php
@@ -325,8 +325,9 @@
       <span style="display: block;"><b>Type:</b> Sparkling Sweet White Wine</span>
       <br>
       <?php
-      include('db_connect.php')
-      db_connect();
+        include('db_connect.php');
+
+        db_connect();
       ?>
       <p>De-stemming and crushing, followed by maceration on the skins for several hours prior to squeezing in horizontal presses. Off-skins fermentation for 20 days at a controlled temperature of approximately 16° C. The formation of a light mousse in pressure tanks with the addition of selected yeasts to reach an excess pressure of 1.5 Atm is followed by stabilization and refrigeration at -4° C. Before bottling, the wine undergoes microfiltration, and it is then left to refine in the bottle for several weeks.</p>
     </div>
